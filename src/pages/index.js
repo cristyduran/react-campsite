1ac1073dc40ba22edb6145b6638291c406210aa2
Cristy Duran <duran_cris43@yahoo.com>
2023-05-08 09:40:37 -0700

    contact form

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import { default as ContactPage } from './ContactPage';
+import { default as HomePage } from './HomePage';
+import { default as AboutPage } from './AboutPage';
+import { default as CampsiteDirectoryPage } from './CampsiteDirectoryPage';
+import { default as CampsiteDetailPage } from './CampsiteDetailPage';
